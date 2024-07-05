6cdbe6aeda5d1c2f357b228998f8f03762bd00c4
sanan <sanan3946@gmail.com>
2024-07-06 03:54:16 +0530

    project init

---
+++ src/main/java/com/springboot/webflux/SpringBoot_WebFlux/SpringBootWebFluxApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.webflux.SpringBoot_WebFlux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebFluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebFluxApplication.class, args);
+	}
+
+}
